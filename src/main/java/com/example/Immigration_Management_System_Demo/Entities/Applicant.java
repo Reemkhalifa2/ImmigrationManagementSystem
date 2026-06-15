cf939c2c3aa260747422546d78f916eb8989181b
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 16:34:24 +0400

    Add validation annotations for passportNumber and nationality fields

--- src/main/java/com/example/Immigration_Management_System_Demo/Entities/Applicant.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Entities/Applicant.java
@@ -1,6 +1,7 @@
 package com.example.Immigration_Management_System_Demo.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,9 @@ public class Applicant extends Person{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Passport cannot be blank.")
     private String passportNumber;
+    @NotBlank(message = "Nationality cannot be blank.")
     private String nationality;
     private Boolean criminalRecord;
     @OneToMany
