e0e87f61db51f938c6a9be9855e09294d76673ea
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 15:14:48 +0400

    Add GenericException class for custom exception handling

---
+++ src/main/java/com/example/Immigration_Management_System_Demo/Exceptions/GenericException.java
@@ -0,0 +1,7 @@
+package com.example.Immigration_Management_System_Demo.Exceptions;
+
+public class GenericException extends RuntimeException {
+    public GenericException(String message) {
+        super(message);
+    }
+}
