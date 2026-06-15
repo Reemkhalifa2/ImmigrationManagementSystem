9c13156a076ca180c75a35931eb94e805948477b
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 15:14:58 +0400

    Add InterviewRepository interface for managing interview data

---
+++ src/main/java/com/example/Immigration_Management_System_Demo/Repository/InterviewRepository.java
@@ -0,0 +1,14 @@
+package com.example.Immigration_Management_System_Demo.Repository;
+
+import com.example.Immigration_Management_System_Demo.Entities.Interview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InterviewRepository extends JpaRepository<Interview , Long> {
+    List<Interview> findByOfficerIdAndInterviewDate(Long officerId, String date);
+    List<Interview> findByApplicantId(Long applicantId);
+    List<Interview> findByOfficerId(Long officerId);
+    List<Interview> findByStatus(String status);
+    List<Interview> findByApplicantIdAndStatus(Long applicantId, String status);
+}
