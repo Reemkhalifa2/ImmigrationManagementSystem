c85ee159961f010a51e27e81ad55d9a34c9149b4
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 15:14:33 +0400

    Add CenterRepository interface for ImmigrationCenter entity

---
+++ src/main/java/com/example/Immigration_Management_System_Demo/Repository/CenterRepository.java
@@ -0,0 +1,15 @@
+package com.example.Immigration_Management_System_Demo.Repository;
+
+import com.example.Immigration_Management_System_Demo.Entities.ImmigrationCenter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CenterRepository extends JpaRepository<ImmigrationCenter , Long> {
+    List<ImmigrationCenter> findByType(String type);
+    List<ImmigrationCenter> findByLocationCountry(String locationCountry);
+}
+
+
+
+
