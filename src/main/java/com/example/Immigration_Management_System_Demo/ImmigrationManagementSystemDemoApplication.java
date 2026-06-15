b1ca077ae7bc219bee4cb5d00255bc58737953d4
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 09:24:28 +0400

    first commit

---
+++ src/main/java/com/example/Immigration_Management_System_Demo/ImmigrationManagementSystemDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Immigration_Management_System_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmigrationManagementSystemDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmigrationManagementSystemDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Immigration_Management_System_Demo/ImmigrationManagementSystemDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Immigration_Management_System_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmigrationManagementSystemDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
