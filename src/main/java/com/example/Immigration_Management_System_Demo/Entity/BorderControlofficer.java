609d268d4cf4980203737498841520d5024cf27c
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 10:47:44 +0400

    Add annotations to BorderControlofficer class for Lombok and JPA

--- src/main/java/com/example/Immigration_Management_System_Demo/Entity/BorderControlofficer.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Entity/BorderControlofficer.java
@@ -1,7 +1,14 @@
 package com.example.Immigration_Management_System_Demo.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class BorderControlofficer extends ImmigrationOfficer{
     private String assignedCheckpoint;
