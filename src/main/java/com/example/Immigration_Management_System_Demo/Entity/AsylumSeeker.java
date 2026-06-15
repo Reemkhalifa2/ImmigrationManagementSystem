6f1c9dc70ff2c8569fdc747651d05607a45e981f
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 10:47:17 +0400

    Add annotations and imports for AsylumSeeker entity

--- src/main/java/com/example/Immigration_Management_System_Demo/Entity/AsylumSeeker.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Entity/AsylumSeeker.java
@@ -1,10 +1,16 @@
 package com.example.Immigration_Management_System_Demo.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class AsylumSeeker extends Applicant{
-
     private String countryOfOrigin;
     private String sponsorOrganization;
 }
