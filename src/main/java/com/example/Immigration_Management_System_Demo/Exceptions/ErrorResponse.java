0252b3b8e8cfe7d6af343d43c35ec1a8b9f1f2c6
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 15:14:43 +0400

    Add ErrorResponse class for standardized error handling

---
+++ src/main/java/com/example/Immigration_Management_System_Demo/Exceptions/ErrorResponse.java
@@ -0,0 +1,30 @@
+package com.example.Immigration_Management_System_Demo.Exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    public ErrorResponse(Date timestamp, int statusCode, HttpStatus status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.statusCode = statusCode;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    private Date timestamp;
+    private int statusCode;
+    private HttpStatus status;
+    private String error;
+    private String message;
+    private String path;
+
+}
