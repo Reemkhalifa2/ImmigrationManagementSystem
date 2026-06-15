ad42e86cd4f17d4a854da860ec913ca631263e4c
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 16:34:38 +0400

    Add exception handler for GenericException in GlobalExceptionHandler

--- src/main/java/com/example/Immigration_Management_System_Demo/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Exceptions/GlobalExceptionHandler.java
@@ -2,13 +2,27 @@ package com.example.Immigration_Management_System_Demo.Exceptions;
 
 import org.springframework.http.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Date;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(GenericException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(GenericException ex , WebRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(
+                new Date(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND ,
+                "Resource Not Found",
+                ex.getMessage(),
+                request.getDescription(false).replace("uri",""));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
 
 
 }
