eba4b7de4e6e93141054d6d33eece16a687727e5
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-15 15:14:52 +0400

    Add GlobalExceptionHandler for centralized exception handling

---
+++ src/main/java/com/example/Immigration_Management_System_Demo/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.Immigration_Management_System_Demo.Exceptions;
+
+import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
