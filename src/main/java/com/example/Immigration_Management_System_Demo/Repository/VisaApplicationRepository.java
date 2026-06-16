f2a2226b17a9292995b2986522c391d9e00652fb
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-16 15:54:33 +0400

    Add methods to retrieve visas by applicant and status

--- src/main/java/com/example/Immigration_Management_System_Demo/Repository/VisaApplicationRepository.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Repository/VisaApplicationRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface VisaApplicationRepository extends JpaRepository<VisaApplication, Long> {
+    List<VisaApplication> getVisasByApplicant(Long applicantId);
+    List<VisaApplication> findByStatusIgnoreCase(String status);
 
 }
