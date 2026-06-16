30f8d3ceed0b3d9a812126c5ca813c104bb9d596
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-16 15:54:11 +0400

    Add interview scheduling and management methods to InterviewRepository

--- src/main/java/com/example/Immigration_Management_System_Demo/Repository/InterviewRepository.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Repository/InterviewRepository.java
@@ -6,5 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InterviewRepository extends JpaRepository<Interview , Long> {
+    Interview scheduleInterview(Long applicantId,
+                                Long officerId,
+                                String date);
+
+    Interview completeInterview(Long interviewId);
+
+    Interview cancelInterview(Long interviewId);
+
+    List<Interview> getOfficerSchedule(Long officerId,
+                                       String date);
 
 }
