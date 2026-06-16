a275ee2575b3ed955c321292bbf615153e3b9d53
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-16 10:31:50 +0400

    Add custom query method to OfficerRepository for active ImmigrationOfficer by ID

--- src/main/java/com/example/Immigration_Management_System_Demo/Repository/OfficerRepository.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Repository/OfficerRepository.java
@@ -2,13 +2,13 @@ package com.example.Immigration_Management_System_Demo.Repository;
 
 import com.example.Immigration_Management_System_Demo.Entities.ImmigrationOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OfficerRepository extends JpaRepository<ImmigrationOfficer , Long> {
 
-    List<ImmigrationOfficer> findByRank(String rank);
-    List<ImmigrationOfficer> findByRankAndClearanceLevelGreaterThanEqual(String rank, int clearanceLevel);
-    List<ImmigrationOfficer> findByActive(boolean active);
-    List<ImmigrationOfficer> findByCenterId(Long centerId);
+    @Query("SELECT i from ImmigrationOfficer i where i.id=:id And i.active = true")
+    ImmigrationOfficer getById(@Param("id") Long id);
 }
