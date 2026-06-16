dfaf49afcb808d3381aba28294698332bf88804a
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-16 11:08:05 +0400

    Complete Center Controller

--- src/main/java/com/example/Immigration_Management_System_Demo/Controllers/CenterController.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Controllers/CenterController.java
@@ -1,14 +1,9 @@
 package com.example.Immigration_Management_System_Demo.Controllers;
 
-import com.example.Immigration_Management_System_Demo.Entities.Applicant;
-import com.example.Immigration_Management_System_Demo.Entities.AsylumSeeker;
 import com.example.Immigration_Management_System_Demo.Entities.ImmigrationCenter;
-import com.example.Immigration_Management_System_Demo.Services.ApplicantService;
 import com.example.Immigration_Management_System_Demo.Services.CenterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("centers")
