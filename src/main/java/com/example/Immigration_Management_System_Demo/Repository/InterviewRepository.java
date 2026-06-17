955e6562e341d957bff3d6a47e540a4728070e90
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-17 10:20:49 +0400

    Rename method parameter for clarity in InterviewRepository

--- src/main/java/com/example/Immigration_Management_System_Demo/Repository/InterviewRepository.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Repository/InterviewRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InterviewRepository extends JpaRepository<Interview , Long> {
-    List<Interview> findByOfficerIdAndInterviewDate(Long officerId, String interviewDate);
+    List<Interview> findByImmigrationOfficerIdAndInterviewDate(Long officerId, String interviewDate);
 
 }
