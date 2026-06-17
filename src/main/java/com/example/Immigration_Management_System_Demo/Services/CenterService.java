459531adc9de803fbca8827f240a5ca0948beb0d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-17 10:00:37 +0400

    Refactor error handling to use GenericException for null checks

--- src/main/java/com/example/Immigration_Management_System_Demo/Services/CenterService.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Services/CenterService.java
@@ -2,6 +2,7 @@ package com.example.Immigration_Management_System_Demo.Services;
 
 import DTO.CenterDTO;
 import com.example.Immigration_Management_System_Demo.Entities.ImmigrationCenter;
+import com.example.Immigration_Management_System_Demo.Exceptions.GenericException;
 import com.example.Immigration_Management_System_Demo.Repository.CenterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,12 +18,16 @@ public class CenterService {
     CenterRepository centerRepository;
 
     public ImmigrationCenter register(ImmigrationCenter immigrationCenter){
+        if(immigrationCenter == null){
+            throw new GenericException(
+                    "Immigration Center cannot be null");
+        }
         return centerRepository.save(immigrationCenter);
     }
 
     public CenterDTO search(Long id) {
         if(id == null){
-            throw new RuntimeException("Id cannot be null");
+            throw new GenericException("Id cannot be null");
         }
         return CenterDTO.convertToDTO(centerRepository.getById(id));
 
