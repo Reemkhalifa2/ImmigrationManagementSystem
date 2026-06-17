1497b0fd60665527d1676cdf6d4a986748cd0a30
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-17 09:47:51 +0400

    Add method to retrieve ImmigrationCenter by ID

--- src/main/java/com/example/Immigration_Management_System_Demo/Repository/CenterRepository.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Repository/CenterRepository.java
@@ -2,10 +2,16 @@ package com.example.Immigration_Management_System_Demo.Repository;
 
 import com.example.Immigration_Management_System_Demo.Entities.ImmigrationCenter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CenterRepository extends JpaRepository<ImmigrationCenter , Long> {
+    @Query("SELECT c from ImmigrationCenter where id=:id")
+    ImmigrationCenter getById(@Param("id") Long id);
+
+
 
 }
 
