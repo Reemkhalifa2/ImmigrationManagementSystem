0fb8b22d4cfc0469c442301e7e622a6891398e1e
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-17 09:48:19 +0400

    Add validation annotations for interviewDate and status fields

--- src/main/java/com/example/Immigration_Management_System_Demo/Entities/Interview.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Entities/Interview.java
@@ -1,6 +1,9 @@
 package com.example.Immigration_Management_System_Demo.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +18,14 @@ public class Interview {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "Interview date is required")
     private String interviewDate;
+    @NotBlank(message = "Status is required")
+    @Pattern(
+            regexp = "^(SCHEDULED|COMPLETED|CANCELLED)$",
+            message = "Status must SCHEDULED|COMPLETED|CANCELLED"
+    )
     private String status;
     private String purpose;
     @ManyToOne
