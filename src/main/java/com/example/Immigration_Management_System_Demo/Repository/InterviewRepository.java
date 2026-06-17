e6256ee9a14519d05dfe901076c644f8c889bbd7
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-17 09:48:25 +0400

    Remove unused methods from InterviewRepository interface

--- src/main/java/com/example/Immigration_Management_System_Demo/Repository/InterviewRepository.java
+++ src/main/java/com/example/Immigration_Management_System_Demo/Repository/InterviewRepository.java
@@ -6,17 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InterviewRepository extends JpaRepository<Interview , Long> {
-    Interview scheduleInterview(Long applicantId,
-                                Long officerId,
-                                String date);
-
-    Interview completeInterview(Long interviewId);
-
-    Interview cancelInterview(Long interviewId);
-
-    List<Interview> getOfficerSchedule(Long officerId,
-                                       String date);
     List<Interview> findByOfficerIdAndInterviewDate(Long officerId, String interviewDate);
-
 
 }
